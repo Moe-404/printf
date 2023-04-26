5aedcc6558e8a0afd86ce10670eb58e891d2ef3b
nada689 <120287148+nada689@users.noreply.github.com>
2023-04-26 05:18:17 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -33,8 +33,7 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+			printed = handle_print(format, &i, list, buffer,flags, width, precision, size);
 			if (printed == -1){
 				return (-1);
 			}
@@ -56,7 +55,4 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 	}
 	*buff_ind = 0;
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
 }
